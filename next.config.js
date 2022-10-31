267ed081443afdc6714e9532293be3e460bc6591
srj101 <salim15-3117@diu.edu.bd>
2022-10-31 11:51:17 +0600

    Init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-const removeImports = require('next-remove-imports')();
+const removeImports = require("next-remove-imports")();
 
 module.exports = removeImports({
   reactStrictMode: true,
   swcMinify: true,
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
 });
